84b5ed7de8e5f7066b66fd000a57a3461b2f8a22
Chris Whitman <chris@uptrending.com>
2012-01-08 08:11:17 -0800

    Comment in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   namespace :admin do
-     match "social-sharing" => "social_sharing#index", :as => :social_sharing
-     match "social-sharing/edit" => "social_sharing#edit", :as => :edit_social_sharing
-     match "social-sharing/update" => "social_sharing#update", :as => :update_social_sharing
+    # resources :social_sharing isn't playing nicely in our extension
+    match "social-sharing" => "social_sharing#index", :as => :social_sharing
+    match "social-sharing/edit" => "social_sharing#edit", :as => :edit_social_sharing
+    match "social-sharing/update" => "social_sharing#update", :as => :update_social_sharing
   end
 end
